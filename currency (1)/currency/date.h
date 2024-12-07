e31933c6302e323af7be44cf81c0f8019d628b85
Pavel Donskikh <122727746+pashokioronetkin@users.noreply.github.com>
2024-12-08 01:07:07 +0300

    Add files via upload

---
+++ currency (1)/currency/date.h
@@ -0,0 +1,12 @@
+//date.h
+#ifndef DATE_H_INCLUDED
+#define DATE_H_INCLUDED
+
+//Структура для хранения даты
+typedef struct _date {
+    int year; //Год
+    int month; //Месяц
+    int day; //День
+} Date;
+
+#endif //DATE_H_INCLUDED
